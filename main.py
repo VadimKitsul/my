88d25acf5e18069a4b6fa47950bc3c4aca04726b
vadim <gg1370316@gmail.com>
2023-10-22 11:37:31 -0400

    version 6.0

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ if num % 2 == 0:
     print('Четное')
 else:
     print('Нечётное')
+def get_even(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+for i in range(14000):
+    a = int(input('A'))
+    print(get_even(a))
+
 
